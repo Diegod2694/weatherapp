ec897ae705c0e479efd8b2b20f6bc9aeda10b601
Luis <dparra@lctis.com.ve>
2018-06-01 16:53:58 -0400

    feat: update button

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Location from './Location'
 import WeatherData from './WeatherData'
+import {
+  // CLOUD,
+  // CLOUDY,
+  SUN,
+  // RAIN,
+  // SNOW,
+  WINDY
+} from './../../constants/weathers'
 import './styles.css'
 
-const WeatherLocation = () => (
-  <div className='weatherLocationCont'>
-    <Location city={'Buenos Aires!'} />
-    <WeatherData />
-  </div>
-)
+const data1 = {
+  temperature: 20,
+  weatherState: SUN,
+  humidity: 10,
+  wind: '10 m/s'
+}
+
+const data2 = {
+  temperature: 18,
+  weatherState: WINDY,
+  humidity: 5,
+  wind: '19 m/s'
+}
+
+class WeatherLocation extends Component {
+  state={
+    city: 'Buenos Aires',
+    data: data1
+  }
+
+  handleUpdateClick = () => {
+    this.setState({
+      data: data2
+    })
+  }
+
+  render() {
+    const { city, data } = this.state
+    return (
+      <div className='weatherLocationCont'>
+        <Location city={city} />
+        <WeatherData data={data}/>
+        <button onClick={this.handleUpdateClick}>Actualizar</button>
+      </div>
+    )
+  }
+}
 
 export default WeatherLocation
