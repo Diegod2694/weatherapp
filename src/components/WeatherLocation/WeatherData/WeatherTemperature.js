b9821480921ab3cea8f7d6a47e119ed41d2d6ac3
Luis <dparra@lctis.com.ve>
2018-06-01 16:16:36 -0400

    basic styling

--- src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -30,13 +30,14 @@ const stateToIconName = weatherState => {
 }
 
 const getWeatherIcon = weatherState => {
-  return (<WeatherIcons name={stateToIconName(weatherState)} size='2x'/>)
+  return (<WeatherIcons className='wicon' name={stateToIconName(weatherState)} size='4x'/>)
 }
 
 const WeatherTemperature = ({ temperature, weatherState }) => (
-  <div>
+  <div className='weatherTemperatureCont'>
     { getWeatherIcon(weatherState) }
-    <span>{`${temperature} C°`}</span>
+    <span className='temperature'>{temperature}</span>
+    <span className='weathertype'>C°</span>
   </div>
 )
 
