f05d9275b41e807ec67981d02415094ea78def9f
Diego Parra <dparra@lctis.com.ve>
2018-06-02 20:06:42 -0400

    service layer created and connection with api established

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import Location from './Location'
 import WeatherData from './WeatherData'
-// import transformWeather from './../../services/transformWeather'
+import transformWeather from './../../services/transformWeather'
 import { SUN } from './../../constants/weathers'
 
 import './styles.css'
 
-// const location  = 'Buenos Aires,ar'
-// const api_key = 'b229552c29857c4b843acf9d34e2634f'
-// const api_weather = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`
+const location  = 'Buenos Aires,ar'
+const api_key = 'b229552c29857c4b843acf9d34e2634f'
+const api_weather = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`
 
 const data1 = {
   temperature: 20,
@@ -16,49 +16,23 @@ const data1 = {
   humidity: 10,
   wind: '10 m/s'
 }
-
-const data2 = {
-  temperature: 20,
-  weatherState: SUN,
-  humidity: 10,
-  wind: '10 m/s'
-}
-class WeatherLocation extends Component {
-  constructor (props) {
-    super(props)
-    this.state={ 
+export default class WeatherLocation extends Component {
+  state={ 
       city: 'Buenos Aires',
       data: data1
     }
-    console.log('constructor');
-  }
-  
-  componentWillMount () {
-    console.log('componentWillMount');
-  }
-  
-  componentDidMount () {
-    console.log('componentDidMount');
-  }
-  
-  componentWillUpdate () {
-    console.log('componentWillUpdate');
-  }
-
-  componentDidUpdate () {
-    console.log('componentDidUpdate');
-  }
    
   handleUpdateClick = () => {
-    console.log('actualizado');
-    this.setState({
-      date: data2
+    fetch(api_weather).then(data => {
+      console.log(data)
+      return data.json()
+    }).then( weather_data => {
+      const data = transformWeather(weather_data)
+      this.setState({data})
     })
   }
 
-  render = () => {
-    console.log('render');
-    
+  render() {
     const { city, data } = this.state
     return (
       <div className='weatherLocationCont'>
@@ -69,5 +43,3 @@ class WeatherLocation extends Component {
     )
   }
 }
-
-export default WeatherLocation
