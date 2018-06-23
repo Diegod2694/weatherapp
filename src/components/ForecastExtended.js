66f93477985fa64de1358f3c3272ecd278593e93
Diego Parra <diegod2694@gmail.com>
2018-06-23 15:19:38 -0400

    WIP:  connected to API

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import ForecastItem from './ForecastItem'
+import transformForecast from './../services/transformForecast'
 import './styles.css'
 
-import ForecastItem from './ForecastItem'
-
-const days = [
-  'Lunes',
-  'Martes',
-  'Miercoles',
-  'Jueves',
-  'Viernes'
-]
+const url = 'http://api.openweathermap.org/data/2.5/forecast'
+const api_key = 'b229552c29857c4b843acf9d34e2634f'
 
 export default class ForecastExtended extends Component {
   state = {
     forecastData: null
   }
   
-  renderForecastItemDays(days) {
-    return 'Render Items'
-    // return days.map( day => <ForecastItem weekDay={day} hour={10}></ForecastItem>)
+  componentDidMount() {
+    const { city } = this.props
+    this.updateCity(city)
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    const { city } = this.props    
+     if (nextProps.city !== city) {
+       this.setState({ forecastData: null })
+       this.updateCity(nextProps.city)
+     }
+  }
+  
+
+  updateCity = city => {
+    const url_forecastData = `${url}?q=${city}&appid=${api_key}`
+
+    fetch(url_forecastData).then(
+      data => data.json()
+    ).then(
+      weather_data => {
+        console.log(weather_data)
+        const forecastData = transformForecast(weather_data)
+        console.log('forecastData', forecastData)
+        this.setState({ forecastData })
+      }
+    )
+  }
+
+  renderForecastItemDays(forecastData) {
+    return forecastData.map( forecast => (
+      <ForecastItem
+        key={`${forecast.weekDay}${forecast.hour}`}
+        weekDay={forecast.weekDay}
+        hour={forecast.hour}
+        data={forecast.data}></ForecastItem>
+    ))
   }
 
   renderProgress = () => {
@@ -34,7 +63,7 @@ export default class ForecastExtended extends Component {
         <h2 className='forecast-title'>Pronostico extendido para {city}</h2>
         { 
          forecastData ?
-          this.renderForecastItemDays(days)
+          this.renderForecastItemDays(forecastData)
           : this.renderProgress()
         }
       </div>
